3d1e8587436225b47bc07f887b07d32118a045d3
hamzashabbir517 <shabbirhamza517@gmail.com>
2022-05-30 03:38:40 +0500

    Final Files Added

--- verilog/rtl/FPU/FPU_FSM_Control_Decode.v
+++ verilog/rtl/FPU/FPU_FSM_Control_Decode.v
@@ -6,49 +6,25 @@ module FPU_FSM(clk,rst_l,Active_Process,Activation_Signal,Memory_Activation,PC,I
   output reg[31:0]PC;
   output [31:0]Instruction_out;
 
-  reg [1:0]State;
-  wire [1:0]Next_State;
+  reg [2:0]State;
+  wire [2:0]Next_State;
+  wire [4:0]Input_Opcode;
   wire Exception;
 
-  /*
-  sfpu[0] = Fadd
-  sfpu[1] = Fsubb
-  sfpu[2] = Fmul
-  sfpu[3] = Fdiv
-  sfpu[4] = Fsqrt
-  sfpu[5] = Fmin
-  sfpu[6] = Fmax
-  sfpu[7] = Fmvx
-  sfpu[8] = Fmvf
-  sfpu[9] = feq
-  sfpu[10] = flt
-  sfpu[11] = fle
-  sfpu[12] = Fmadd
-  sfpu[13] = Fmsubb
-  sfpu[14] = FCVT.W.P
-  sfpu[15] = FCVT.P.W
-  sfpu[16] = Fnmsubb
-  sfpu[17] = Fnmadd
-  sfpu[18] = fsgnj
-  sfpu[19] = fsgnjn
-  sfpu[20] = fsgnjx
-  sfpu[21] = fclass
-  sfpu[22] = unsign
-  sfpu[23] = sign
-  */
- 
-  //assign Multi_Cycle = 1'b0;
-  assign Memory_Activation = (((~Next_State[1]) & (Next_State[0])) & (Active_Process));
-  assign Next_State[0] = (((~State[0]) & (State[1] | Active_Process)) | ((State[1] & State[0]) & ((~Activation_Signal) | (Activation_Signal & (~Multi_Cycle)))));
-  assign Next_State[1] = (((~State[1]) & (State[0])) | ((State[1]) & (~State[0])) | (State[1] & State[0] & (~Activation_Signal)));
-  assign Instruction_out = (~rst_l) ? 32'h00000000 : (State == 2'b10) ? Instruction : 32'h00000000;
+  assign Memory_Activation = (((~Next_State[2]) & (~Next_State[1]) & (Next_State[0])) & (Active_Process));
+  assign Next_State[0] = ((((~State[2]) & (~State[0])) & (State[0] | Active_Process)) | ((State[2]) & (~State[1]) & (~State[0]) & (~Multi_Cycle) & (Activation_Signal)));
+  assign Next_State[1] = (((~State[2]) & (~State[1]) & (State[0])) | ((~State[2]) & (State[1]) & (~State[0])));
+  assign Next_State[2] = (((~State[2]) & (State[1]) & (State[0])) | ((State[2]) & (~State[1]) & (~State[0]) & (~Activation_Signal)));
+  assign Instruction_out = (~rst_l) ? 32'h00000000 : (State == 3'b010) ? Instruction : 32'h00000000;
   always @(posedge clk)
-    PC <= (~rst_l) ? 32'h00000000 : ((State == 2'b01) ? (PC[31:0] + 4'h4) : (Instruction == 32'h00000010) ? 32'h00000000 : PC);
+    PC <= (~rst_l) ? 32'h00000000 : ((State == 3'b001) ? (PC[31:0] + 4'h4) : (Instruction == 32'h00000010) ? 32'h00000000 : PC);
 
   //sequential State register block
   always @(posedge clk)
-    State <= (~rst_l) ? 2'b00 : Next_State;
+    State <= (~rst_l) ? 3'b000 : Next_State;
 
+
+   
 
   //combinational State assignment block  
   /*always @(posedge clk)
